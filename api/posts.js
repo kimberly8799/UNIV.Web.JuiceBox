a5b27ea4d9474e6c02c3cb018b0da9d72ffadb60
Kimberly Phan <kimberly.t.phan01@sjsu.edu>
2023-05-22 11:59:00 -0700

    finished writing POST/api/posts

--- api/posts.js
+++ api/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
+const { getAllPosts, createPost } = require('../db');
 const postsRouter = express.Router();
 
 postsRouter.use((req, res, next) => {
@@ -15,5 +16,39 @@ postsRouter.get('/', async (req, res) => {
     "posts": []
   });
 });
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = req.user.id;
+    postData.title = title;
+    postData.content = content;
+    
+    const post = await createPost(postData);
+
+    if (post) {
+      res.send({ post });
+    } else {
+      next({
+        name: 'CreatePostError',
+        message: "Error creating post"
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  } 
+  //res.send({ message: 'under construction' });
+});
+
+
+
 
 module.exports = postsRouter;
